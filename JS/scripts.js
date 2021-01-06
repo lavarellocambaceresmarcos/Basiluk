0cd31d5603dd9f2233b3646f70cc40862bb858eb
lavarellocambaceresmarcos <lavarellomarcos@gmail.com>
2021-01-06 15:46:40 -0300

    primera subida-todo super básico-desafíos 1 y 2 de JS

---
+++ JS/scripts.js
@@ -0,0 +1,20 @@
+alert ("Bienvenido, porfavor llene los datos consiguientes");
+
+var nombre = prompt ("ingrese su nombre y apellido");
+
+var edad = parseInt(prompt ("ingrese su edad"));
+
+var numeroDePrueba = parseInt(prompt("ingrese número de prueba"));
+
+if (edad >= 18) {
+    alert ("Hola " + nombre)}
+     else { 
+        alert ("usted es menor de edad, regrese en unos años")
+
+        };
+
+if ((numeroDePrueba >=10) && (numeroDePrueba <= 50)) {
+    alert ("genial!")}
+        else {
+            alert ("Señor, usted es diabólico")
+        };
